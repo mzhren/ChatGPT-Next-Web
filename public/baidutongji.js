32c8dcdf2f5eb856c737dffc44fd79e5341ea211
mzhren <flowerszhong@hotmail.com>
2023-05-24 22:19:51 +0800

    add baidutongji

---
+++ public/baidutongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?56d9368d933375bc893070c60ab45fd2";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
